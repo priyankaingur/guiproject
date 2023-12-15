cc3d63a948eaeff0fe013aa82c04d5400e2a41b4
priyankaingur <p_thangamuthu@u.pacific.edu>
2023-12-14 23:36:38 -0800

    implemented applicable sonarLint recommentations

--- db/insert.py
+++ db/insert.py
@@ -5,14 +5,21 @@ from logger.log import Logger
 # including vault is outside scope of this project and for simplicity, dummy connection password is used
 
 
-# connect = pymysql.connect(host="localhost", user="root", passwd="root", database="default")
+connect = pymysql.connect(host="localhost", user="root", passwd="root", database="default")
 # uncomment above for initial run and validation and use below for more secured connection
-connect = pymysql.connect(host="localhost", user="root", passwd="root", database="default", ssl_cert="root-cert.pem",
-                          ssl_key="root-key.pem", ssl_ca="ca.pem")
+# connect = pymysql.connect(host="localhost", user="root", passwd="root", database="default", ssl_cert="root-cert.pem",
+#                           ssl_key="root-key.pem", ssl_ca="ca.pem")
 cursor = connect.cursor()
 
 # declare logger object
 logger_object = Logger("T:\Priyanka\Semester-3\COMP-270 Secure Software Systems\guiproject\gui.log")
+
+
+# defining constant to re-use in insert statement
+
+# encrypt = "\"', aes_encrypt('\""
+# key = " \"','key'), \""
+# key_close = "\"' ,'key'))\""
 
 
 def load_user(**load):
@@ -39,6 +46,13 @@ def load_pii(**load):
         cursor.execute("insert into pii(id, user_name, user_email, address, contact_number, date_of_birth, social_security_number, driver_license)"
             "values('" + str(
                 uid) + "', aes_encrypt('" + fernet.decrypt(enc_user_name).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_email).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_address).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_contact).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_dob).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_ssn).decode() + "','key'), " + " aes_encrypt('" + fernet.decrypt(enc_user_dl).decode() + "' ,'key'))")
+        # defining a constant instead of duplicating literal is not compatible with aws_encryption function; hence
+        # ignoring the sonarLint recommendation cursor.execute("insert into pii(id, user_name, user_email, address,
+        # contact_number, date_of_birth, social_security_number, driver_license)" "values('" + str( uid) + encrypt +
+        # fernet.decrypt(enc_user_name).decode() + key + encrypt + fernet.decrypt(enc_user_email).decode() + key +
+        # encrypt + fernet.decrypt(enc_user_address).decode() + key + encrypt + fernet.decrypt(
+        # enc_user_contact).decode() + key + encrypt + fernet.decrypt(enc_user_dob).decode() + key + encrypt +
+        # fernet.decrypt(enc_user_ssn).decode() + key + encrypt + fernet.decrypt(enc_user_dl).decode() + key_close)
         connect.commit()
         logger_object.info("data has been pushed to pii table " + str(uid))
     except Exception as e:
